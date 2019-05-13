0f6dc7369560d336fece03d26ddf0b9bc83cc523
gabrielleschoepping <50590927+gabrielleschoepping@users.noreply.github.com>
2019-05-13 13:35:57 -0300

    programa de dados - 1a versão

---
+++ dados.py
@@ -0,0 +1,13 @@
+# Meu primeiro programa
+from random import randint #importa a função de gerar número aleatório
+
+print("Olá 1info2!") 
+
+dado1 = randint(1, 6) #gera um número entre 1 e 6 aleatoriamente
+print("dado1:", dado1)
+dado2 = randint(1, 6)
+print("dado2:", dado2)
+dado3 = randint(1, 6)
+print("dado2:", dado3)
+dado4 = randint(1, 6)
+print("dado4:", dado4)
